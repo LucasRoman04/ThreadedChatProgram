2d08e3d32d7aeabd97b93f5cac01c5974a240bfd
LucasRoman04 <w0476219@nscc.ca>
2023-10-10 11:46:59 -0300

    Ported code

--- Assignment_1/Form1.Designer.cs
+++ Assignment_1/Form1.Designer.cs
@@ -32,19 +32,20 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.networkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.gameLabel = new System.Windows.Forms.Label();
+            this.exitMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectMenuOption = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.sendButton = new System.Windows.Forms.Button();
             this.messageGroupBox = new System.Windows.Forms.GroupBox();
             this.messageTextBox = new System.Windows.Forms.TextBox();
-            this.conversationGroupBox = new System.Windows.Forms.GroupBox();
+            this.convGroupBox = new System.Windows.Forms.GroupBox();
             this.convTextBox = new System.Windows.Forms.TextBox();
+            this.serverMenuOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientMenuOption = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.messageGroupBox.SuspendLayout();
-            this.conversationGroupBox.SuspendLayout();
+            this.convGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -54,14 +55,14 @@
             this.networkToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(968, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // gameToolStripMenuItem
             // 
             this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
+            this.exitMenuOption});
             this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
             this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.gameToolStripMenuItem.Text = "Game";
@@ -69,42 +70,31 @@
             // networkToolStripMenuItem
             // 
             this.networkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.connectToolStripMenuItem,
-            this.disconnectToolStripMenuItem});
+            this.connectMenuOption,
+            this.disconnectMenuOption,
+            this.serverMenuOption,
+            this.clientMenuOption});
             this.networkToolStripMenuItem.Name = "networkToolStripMenuItem";
             this.networkToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
             this.networkToolStripMenuItem.Text = "Network";
             // 
-            // exitToolStripMenuItem
+            // exitMenuOption
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitMenuOption.Name = "exitMenuOption";
+            this.exitMenuOption.Size = new System.Drawing.Size(180, 22);
+            this.exitMenuOption.Text = "Exit";
             // 
-            // connectToolStripMenuItem
+            // connectMenuOption
             // 
-            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
-            this.connectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectMenuOption.Name = "connectMenuOption";
+            this.connectMenuOption.Size = new System.Drawing.Size(180, 22);
+            this.connectMenuOption.Text = "Connect";
             // 
-            // disconnectToolStripMenuItem
+            // disconnectMenuOption
             // 
-            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
-            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.disconnectToolStripMenuItem.Text = "Disconnect";
-            // 
-            // gameLabel
-            // 
-            this.gameLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.gameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameLabel.ForeColor = System.Drawing.SystemColors.Control;
-            this.gameLabel.Location = new System.Drawing.Point(12, 24);
-            this.gameLabel.Name = "gameLabel";
-            this.gameLabel.Size = new System.Drawing.Size(944, 307);
-            this.gameLabel.TabIndex = 1;
-            this.gameLabel.Text = "Game Goes Here";
-            this.gameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.gameLabel.Click += new System.EventHandler(this.label1_Click);
+            this.disconnectMenuOption.Name = "disconnectMenuOption";
+            this.disconnectMenuOption.Size = new System.Drawing.Size(180, 22);
+            this.disconnectMenuOption.Text = "Disconnect";
             // 
             // contextMenuStrip1
             // 
@@ -113,7 +103,7 @@
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(843, 28);
+            this.sendButton.Location = new System.Drawing.Point(774, 28);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(75, 23);
             this.sendButton.TabIndex = 5;
@@ -124,9 +114,9 @@
             // 
             this.messageGroupBox.Controls.Add(this.messageTextBox);
             this.messageGroupBox.Controls.Add(this.sendButton);
-            this.messageGroupBox.Location = new System.Drawing.Point(19, 345);
+            this.messageGroupBox.Location = new System.Drawing.Point(12, 37);
             this.messageGroupBox.Name = "messageGroupBox";
-            this.messageGroupBox.Size = new System.Drawing.Size(924, 64);
+            this.messageGroupBox.Size = new System.Drawing.Size(858, 64);
             this.messageGroupBox.TabIndex = 6;
             this.messageGroupBox.TabStop = false;
             this.messageGroupBox.Text = "Type your messages here.";
@@ -135,45 +125,57 @@
             // 
             this.messageTextBox.Location = new System.Drawing.Point(6, 30);
             this.messageTextBox.Name = "messageTextBox";
-            this.messageTextBox.Size = new System.Drawing.Size(818, 20);
+            this.messageTextBox.Size = new System.Drawing.Size(762, 20);
             this.messageTextBox.TabIndex = 4;
             // 
-            // conversationGroupBox
+            // convGroupBox
             // 
-            this.conversationGroupBox.Controls.Add(this.convTextBox);
-            this.conversationGroupBox.Location = new System.Drawing.Point(19, 425);
-            this.conversationGroupBox.Name = "conversationGroupBox";
-            this.conversationGroupBox.Size = new System.Drawing.Size(924, 304);
-            this.conversationGroupBox.TabIndex = 7;
-            this.conversationGroupBox.TabStop = false;
-            this.conversationGroupBox.Text = "Conversation";
+            this.convGroupBox.Controls.Add(this.convTextBox);
+            this.convGroupBox.Location = new System.Drawing.Point(12, 117);
+            this.convGroupBox.Name = "convGroupBox";
+            this.convGroupBox.Size = new System.Drawing.Size(858, 304);
+            this.convGroupBox.TabIndex = 7;
+            this.convGroupBox.TabStop = false;
+            this.convGroupBox.Text = "Conversation";
             // 
             // convTextBox
             // 
             this.convTextBox.Location = new System.Drawing.Point(7, 20);
             this.convTextBox.Multiline = true;
             this.convTextBox.Name = "convTextBox";
-            this.convTextBox.Size = new System.Drawing.Size(911, 278);
+            this.convTextBox.Size = new System.Drawing.Size(845, 278);
             this.convTextBox.TabIndex = 0;
+            // 
+            // serverMenuOption
+            // 
+            this.serverMenuOption.Name = "serverMenuOption";
+            this.serverMenuOption.Size = new System.Drawing.Size(180, 22);
+            this.serverMenuOption.Text = "Server";
+            // 
+            // clientMenuOption
+            // 
+            this.clientMenuOption.Name = "clientMenuOption";
+            this.clientMenuOption.Size = new System.Drawing.Size(180, 22);
+            this.clientMenuOption.Text = "Client";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(968, 741);
-            this.Controls.Add(this.conversationGroupBox);
+            this.ClientSize = new System.Drawing.Size(902, 453);
+            this.Controls.Add(this.convGroupBox);
             this.Controls.Add(this.messageGroupBox);
-            this.Controls.Add(this.gameLabel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Network Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.messageGroupBox.ResumeLayout(false);
             this.messageGroupBox.PerformLayout();
-            this.conversationGroupBox.ResumeLayout(false);
-            this.conversationGroupBox.PerformLayout();
+            this.convGroupBox.ResumeLayout(false);
+            this.convGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,17 +185,18 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuOption;
         private System.Windows.Forms.ToolStripMenuItem networkToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
-        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.ToolStripMenuItem connectMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem disconnectMenuOption;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.GroupBox messageGroupBox;
         private System.Windows.Forms.TextBox messageTextBox;
-        private System.Windows.Forms.GroupBox conversationGroupBox;
+        private System.Windows.Forms.GroupBox convGroupBox;
         private System.Windows.Forms.TextBox convTextBox;
+        private System.Windows.Forms.ToolStripMenuItem serverMenuOption;
+        private System.Windows.Forms.ToolStripMenuItem clientMenuOption;
     }
 }
 
--- Assignment_1/Form1.cs
+++ Assignment_1/Form1.cs
@@ -26,5 +26,10 @@ namespace Assignment_1
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
